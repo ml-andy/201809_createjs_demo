bf90cb08664c105a8498b5a6b33dce8e6b95d522
Unknown <andy2472625@gmail.com>
2018-09-27 11:45:37 +0800

    zz

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-var canvas, stage, exportRoot;
+console.log('v.1');
+
+var canvas, stage, bgAni, giftAni;
 
 function init() {
 	canvas = document.getElementById("giftAni");
@@ -19,15 +21,48 @@ function handleFileLoad(evt) {
 function handleComplete(evt) {
 	var queue = evt.target;
 	ss["gift_atlas_"] = queue.getResult("gift_atlas_");
-	exportRoot = new lib.BG_mc();
-	exportRoot.setTransform(264,226.1,1,1,0,0,0,-0.5,0);
-
 	stage = new createjs.Stage(canvas);
-	stage.addChild(exportRoot);
-	stage.update();
 
+	bgAni = new lib.BG_mc();
+	bgAni.setTransform(264,226.1,1,1,0,0,0,-0.5,0);
+	stage.addChild(bgAni);
+
+	giftAni = new lib.GIFT_mc();
+	giftAni.setTransform(97.9,205,1,1,0,0,0,-156.1,0);
+	stage.addChild(giftAni);
+	
+	stage.update();
 	createjs.Ticker.setFPS(lib.properties.fps);
 	createjs.Ticker.addEventListener("tick", stage);
 }
 
 init();
+
+
+$('.list_item').on('mousedown', function() {
+	$(this).addClass('press');
+});
+
+$('.list_item').on('mouseup', function() {
+  $(this).removeClass('press');
+  if ($(this).hasClass('on')) $(this).removeClass('on');
+  else {
+    $(this).addClass('on');
+    switch ($(this).index()) {
+      case 0:
+        giftAni.gotoAndPlay(32);
+        break;
+      case 1:
+        giftAni.gotoAndPlay(43);
+        break;
+      case 2:
+        giftAni.gotoAndPlay(56);
+        break;
+      case 3:
+        giftAni.gotoAndPlay(72);
+        break;
+      default:
+        break;
+    }
+  }
+});
